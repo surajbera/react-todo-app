0b3df8b0a377ee783fd1dc6bc63b60ceb4d68897
suraj.be <suraj.be@media.net>
2023-04-02 22:46:08 +0530

    added readme.txt file

---
+++ readme.txt
@@ -0,0 +1,22 @@
+- Remove border class from the delete btn
+- Resources for getting svg icons:
+  - React Icons (https://react-icons.github.io/react-icons/) - React Icons is a popular library of
+    icons that are specifically designed for use in React applications. The library includes icons
+    from popular icon libraries like Font Awesome, Material Icons, and Ionicons, as well as custom
+    icons designed specifically for React.
+  - Remix Icon (https://remixicon.com/) - Remix Icon is a library of open source icons that are
+    designed specifically for modern web and app development. The icons are available in SVG format
+    and can be easily integrated into React apps using the react-remixicon package.
+  - Heroicons (https://heroicons.com/) - Heroicons is a library of open source icons that are
+    designed specifically for use in web and app development. The icons are available in SVG format
+    and can be easily integrated into React apps using the react-heroicons package.
+  - Material Design Icons (https://materialdesignicons.com/) - Material Design Icons is a library of
+    icons designed by Google that are available in SVG format. The library includes icons for a
+    variety of categories, including action, communication, and content, and can be easily
+    integrated into React apps using the @mdi/react package.
+- Since this is a very small project, I will use the Remix Icon library for the icons.
+
+- TODOS:
+  - Don't know why I am getting an error on adding first todo.
+  - Add a feature where same todos can't be added
+  - Don't display the TodoList compnenet if there are no todos
--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,8 +2,6 @@
 import { DeleteIcon } from '../assets/icons'
 
 const TodoCard = ({ todo, onDelete }) => {
-  console.log(todo)
-
   const handleClick = () => {
     onDelete(todo.id)
   }
@@ -23,15 +21,3 @@ const TodoCard = ({ todo, onDelete }) => {
 }
 
 export default TodoCard
-
-/* COMMENT: 
-    - Remove border class from the delete btn
-
-    - Resources for getting svg icons
-      - React Icons (https://react-icons.github.io/react-icons/) - React Icons is a popular library of icons that are specifically designed for use in React applications. The library includes icons from popular icon libraries like Font Awesome, Material Icons, and Ionicons, as well as custom icons designed specifically for React.
-      - Remix Icon (https://remixicon.com/) - Remix Icon is a library of open source icons that are designed specifically for modern web and app development. The icons are available in SVG format and can be easily integrated into React apps using the react-remixicon package.
-      - Heroicons (https://heroicons.com/) - Heroicons is a library of open source icons that are designed specifically for use in web and app development. The icons are available in SVG format and can be easily integrated into React apps using the react-heroicons package.
-      - Material Design Icons (https://materialdesignicons.com/) - Material Design Icons is a library of icons designed by Google that are available in SVG format. The library includes icons for a variety of categories, including action, communication, and content, and can be easily integrated into React apps using the @mdi/react package.
-
-    - Since this is a very small project, I will use the Remix Icon library for the icons.
-*/
