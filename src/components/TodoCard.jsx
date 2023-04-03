672e3dc9ae7531c9bdcff031b628e724a5ab6bd5
suraj.be <suraj.be@media.net>
2023-04-03 15:46:13 +0530

    Added a TodoEdit component

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -19,20 +19,24 @@ export const TodoCard = ({ todo, onDelete }) => {
     setShowEdit(!showEdit)
   }
 
+  const cardInnerClass = showEdit ? 'todo-card__inner todo-card__inner--edit' : 'todo-card__inner'
+
   return (
     <li className='todo-card border' key={todo.id}>
-      <div className='todo-card__inner'>
-        <div className='todo-card__actions d-flex'>
-          <button className='todo-edit-btn border d-flex' onClick={handleEditClick}>
-            <EditIcon />
-          </button>
-          <button className='todo-delete-btn border d-flex' onClick={handleDeleteClick}>
-            <DeleteIcon />
-          </button>
-        </div>
+      <div className={cardInnerClass}>
+        {!showEdit && (
+          <div className='todo-card__actions d-flex'>
+            <button className='todo-edit-btn border d-flex' onClick={handleEditClick}>
+              <EditIcon />
+            </button>
+            <button className='todo-delete-btn border d-flex' onClick={handleDeleteClick}>
+              <DeleteIcon />
+            </button>
+          </div>
+        )}
 
         {!showEdit && <p className='todo-card__title'>{todo.title}</p>}
-        {showEdit && <TodoEdit />}
+        {showEdit && <TodoEdit todo={todo} />}
       </div>
     </li>
   )
--- src/components/TodoEdit.jsx
+++ src/components/TodoEdit.jsx
@@ -1,3 +1,24 @@
-export const TodoEdit = () => {
-  return <div>TodoEdit</div>
+import { useState } from 'react'
+
+export const TodoEdit = ({ todo }) => {
+  const [title, setTitle] = useState(todo.title)
+
+  const handleChange = (evt) => {
+    setTitle(evt.target.value)
+  }
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log(title)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='edit-todo-input' className='edit-todo-label'>
+          Edit Todo:
+        </label>
+        <input type='text' value={title} id='edit-todo-input' onChange={handleChange} />
+      </form>
+    </div>
+  )
 }
